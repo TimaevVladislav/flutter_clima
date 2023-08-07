3c5a3f48684fb47a8f74ae9ec94460385c3012ce
timaevV <timaevvladislav@gmail.com>
2023-08-08 01:10:21 +0600

    add:fetch location

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,18 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void fetchLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: FilledButton(
           onPressed: () {
-            //Get the current location
+            fetchLocation();
           },
           child: Text('Get Location'),
         ),
